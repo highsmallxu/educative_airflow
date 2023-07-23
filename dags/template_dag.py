16f9fa0727b119f26b48096be817b72277a030b6
highsmallxu <highsmallxu@gmail.com>
2023-07-23 21:40:23 +0200

    refactor

--- dags/template_dag.py
+++ dags/template_dag.py
@@ -4,6 +4,10 @@ import pendulum
 from airflow.decorators import dag, task
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 
+
+# Ideally, GOOGLE_APPLICATION_CREDENTIALS should be defined globally instead of in the DAG file
+# This is just an example to simply the setup process
+
 gcp_auth_key = <<gcp_auth>>
 
 with open('auth.json', 'w') as outfile:
