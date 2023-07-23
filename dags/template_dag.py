525216c8903d7922ec2ee23233dcb5c31e339968
highsmallxu <highsmallxu@gmail.com>
2023-07-23 20:56:30 +0200

    add credentials

--- dags/template_dag.py
+++ dags/template_dag.py
@@ -1,6 +1,16 @@
+import json
+import os
 import pendulum
 from airflow.decorators import dag
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+
+gcp_auth_key = <<gcp_auth>>
+
+with open('auth.json', 'w') as outfile:
+    json.dump(gcp_auth_key, outfile)
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "auth.json"
+PROJECT_ID = json.load(open("auth.json","rb"))["quota_project_id"]
 
 @dag(
     schedule="30 4 * * *",
@@ -12,6 +22,7 @@ def educative_dag6():
 
     BigQueryInsertJobOperator(
         task_id="insert_query_job",
+        project_id=PROJECT_ID,
         configuration={
             "query": {
                 "query": "{% include 'sample.sql' %}",
