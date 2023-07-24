4799ab6792a4c0342d51ff2dfffff45f13058da0
highsmallxu <highsmallxu@gmail.com>
2023-07-24 19:06:31 +0200

    update

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,3 +3,4 @@
 python /usercode/setup/setup.py
 echo 'export GOOGLE_APPLICATION_CREDENTIALS=auth.json' >> ~/.bashrc
 echo 'export AIRFLOW_CONN_FRANKFURTER_API=http://https://api.frankfurter.app/' >> ~/.bashrc
+source ~/.bashrc
