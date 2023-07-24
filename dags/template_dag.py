c8139eda07689b9163148f562d7ab8e0a74e739e
highsmallxu <highsmallxu@gmail.com>
2023-07-24 19:10:01 +0200

    auth

--- dags/template_dag.py
+++ dags/template_dag.py
@@ -1,8 +1,15 @@
 import json
+import os
 import pendulum
 from airflow.decorators import dag, task
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 
+gcp_auth_key = <<gcp_auth>>
+
+with open('auth.json', 'w') as outfile:
+    json.dump(gcp_auth_key, outfile)
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "auth.json"
 PROJECT_ID = json.load(open("auth.json","rb"))["quota_project_id"]
 
 @dag(
